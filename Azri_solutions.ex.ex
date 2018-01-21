49fb3fd815ae693f2355290ac905a2c36b8e7749
Mohammed Shoeb <mdshoeb01@gmail.com>
2018-01-21 13:20:39 +0530

    Trying to code in elixir
    This are my initial steps to write the same code in elixir

---
+++ Azri_solutions.ex.ex
@@ -0,0 +1,22 @@
+defmodule River do
+  def get_min(tot,caps,pegs) do
+    curPos=0
+    i=0
+    loop(i,tuple_size(pegs),tot,caps,pegs,curPos)
+
+  end
+  defp loop(iter,size,tot,caps,pegs,curPos) do
+    if(elem(pegs,iter)>curPos and (elem(pegs,iter)-curPos<=cap)) do
+      
+    end
+  end
+end
+tot=IO.gets "Enter the value for tot: "
+cap=IO.gets "Enter the value for cap: "
+pegs={2,4,19,7,5,8}
+
+min_instance=River.get_min(tot,cap,pegs)
+if min_instance<0 do
+  IO.puts "Cannot cross the river"
+else IO.puts "You can cross the river in #{min_instance} seconds"
+end
